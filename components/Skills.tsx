a490e6645e3b65f276f1b3d9141d3b97d1268199
Bhavyansh Jain <jainjain.bhavyansh@gmail.com>
2022-10-26 11:17:05 +0530

    map skills in skill section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -25,6 +25,15 @@ function Skills({ skills }: Props) {
       </h3>
 
       {/* Map icons */}
+      <div className="grid grid-cols-4 gap-5">
+        {skills?.slice(0, skills.length / 2).map((skill) => (
+          <Skill key={skill._id} skill={skill} />
+        ))}
+        {skills?.slice(skills.length / 2, skills.length).map((skill) => (
+          <Skill key={skill._id} skill={skill} directionLeft />
+        ))}
+              
+      </div>
     </motion.div>
   );
 }
